4299f3d6a07a3af4628743c5dd57130094f01485
Erik Trautman <erik.trautman@gmail.com>
2014-10-29 11:55:39 -0700

    remove deets

--- lib/viking.rb
+++ lib/viking.rb
@@ -80,32 +80,3 @@ puts "\nInspecting vikings:\n"
 puts oleg.inspect
 puts sven.inspect
 puts "\n...thanks for playing.\n\n"
-
-# Test that:
-# Viking:
-# Passing a name to a new viking sets that name
-# Passing a health attribute to a new viking sets that health attribute
-# Health cannot be overwritten after it's been set on initialize
-# Weapon starts out `nil` by default
-
-# Bow:
-# A Bow's arrow count is readable
-# Using a Bow reduces arrows
-# Using a Bow with 0 arrows throws an error
-
-# Viking:
-# Picking up a weapon sets it as the Viking's weapon
-# Picking up a non-weapon raises an exception
-# Picking up a new weapon replaces the existing weapon
-# Dropping a weapon leaves the Viking weaponless
-# The `receive_attack` method reduces that Viking's health by the specified amount
-# The `receive_attack` method calls the `take_damage` method (mock!)
-# Attacking another viking causes that viking's health to drop
-# Attacking another viking calls that viking's `take_damage` method (mock!)
-# Attacking with no weapon runs `damage_with_fists` (mock!)
-# Attacking with no weapon deals Fists multiplier * strength damage
-# Attacking with a weapon runs damage_with_weapon (mock!)
-# Attacking with a weapon deals strength * that weapon's multiplier damage
-# Attacking with a bow without enough arrows uses fists instead (mock!)
-# Killing a viking throws an error 
-
